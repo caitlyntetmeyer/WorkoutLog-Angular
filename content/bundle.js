99d499f2e618bc2c10bad9fc3619d8bb4dadbb5c
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-07-09 16:53:50 -0400

    Trying to deploy

--- content/bundle.js
+++ content/bundle.js
@@ -25,7 +25,7 @@
 	config.$inject = [ '$urlRouterProvider' ];
 	app.config(config);
 
-	var API_BASE = location.hostname === "localhost" ? "//localhost:3000/api/" : "//cct-workoutlog.herokuapp.com/api/";
+	var API_BASE = location.hostname === "localhost" ? "//localhost:3000/api/" : "//cctworkoutlogangular.herokuapp.com/api/";
 	// var API_BASE is now dynamic, and WorkoutLog-Angular can run deployed or locally because the ternary operator (?) determines the environment and alters the API_BASE accordingly.
 	app.constant('API_BASE', API_BASE);
 })();
